2c6179f097d0ffdbcb49b47724eca58391834b5f
NestorFabianCabrera <nestor.cabrera@gopenux.com>
2024-09-16 16:15:48 -0500

    Design update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Directive, HostListener, ElementRef, Renderer2  } from '@angular/core';
 import { ApiService } from './services/personaje-service.service';
 import { Character } from './interface/character';
+import {Observable} from "rxjs";
+
+@Directive({
+  selector: 'appHighlight'
+})
+
+export class Hig{
+  mouseover: boolean | undefined;
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+  }
+  @HostListener('mouseover') onMouseOver() {
+    this.mouseover = true;
+    this.renderer.setStyle(this.el.nativeElement, 'background-color', 'yellow');
+  }
+
+  @HostListener('mouseout') onMouseOut() {
+    this.mouseover = false;
+    this.renderer.setStyle(this.el.nativeElement, 'background-color', 'black');
+  }
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  characters: Character[] = [];
 
+export class AppComponent implements OnInit{
+  characters: Character[] = [];
   constructor(private charactersService: ApiService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
     this.charactersService.getUsers().subscribe({
       next: (response: { results: Character[] }) => {
         this.characters = response.results;
@@ -21,5 +43,35 @@ export class AppComponent implements OnInit {
         console.error('Error fetching characters:', err);
       }
     });
+
+    const observable = new Observable<number>((observer) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete();
+
+    });
+
+    observable.subscribe({
+      next: value => console.log(value),
+      complete: () => console.log('Observable ready')
+    });
   }
+
+  @HostListener('document:click', ['$event'])
+  onclick(event: { target: { matches: (arg0: string) => any; }; }) {
+    if(event.target.matches('.editor-div')) {
+      alert('click to editor div')
+    }
+  }
+
+  @HostListener('document:mouseover', ['$event'])
+  mouseover(event: { target: { matches: (arg0: string) => any; }; }) {
+    if(event.target.matches('.editor-div')) {
+      alert('hover to editor div')
+    }
+  }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent, Hig} from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from "@angular/material/card";
 import { CardComponent } from './components/card/card.component';
@@ -15,7 +15,8 @@ import {FormsModule} from "@angular/forms";
   declarations: [
     AppComponent,
     CardComponent,
-    JsonuserComponent
+    JsonuserComponent,
+    Hig
   ],
   imports: [
     BrowserModule,
